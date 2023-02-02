9fce20d8b5320713396962870a026f7979e252e4
jordane-chaves <jordane_chaves@yahoo.com.br>
2023-02-02 18:12:16 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Thu, 02 Feb 2023 15:18:31 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8257e6"
+};
